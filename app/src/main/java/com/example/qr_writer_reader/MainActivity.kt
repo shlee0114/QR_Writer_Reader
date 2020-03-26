5ca0f6fe42ce892f942f2489cb13837227787137
tlgj255 <tlgj255@naver.com>
2020-03-26 18:29:25 +0900

    .

---
+++ app/src/main/java/com/example/qr_writer_reader/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.qr_writer_reader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
